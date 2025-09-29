6aa75b14f51dcf74702b9639cc467b47f6504557
Hercules Fok <fokhdhercules@gmail.com>
2025-09-29 07:16:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function lightTheme() {
-  let 
+  let linkTag = document.getElementById("link");
+  linkTag.href = "lightTheme.css";
+}
+
+function darkTheme() {
+  let linkTag = document.getElementById("link");
+  linkTag.href = "darkTheme.css";
 }
